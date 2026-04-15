5ae4ed52804694ec7fc9d0b84595a198608079c4
Florent BOURASSE <fbourasse@jahia.com>
2026-04-15 21:01:13 +0200

    correction linx reported issues related to Cypress tests
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- tests/cypress/e2e/01-graphqlExtensionWebsites.cy.ts
+++ tests/cypress/e2e/01-graphqlExtensionWebsites.cy.ts
@@ -1,21 +1,20 @@
-import {DocumentNode} from 'graphql'
+import {DocumentNode} from 'graphql';
 
 describe('GraphQL Extension Websites', () => {
-    const TEST_SITE_KEY = 'cypress-test-website'
+    const TEST_SITE_KEY = 'cypress-test-website';
 
-    let createSiteByKey: DocumentNode
-    let deleteSiteByKey: DocumentNode
-    let exportWebsite: DocumentNode
-    let exportAllSites: DocumentNode
-
-    createSiteByKey = require('graphql-tag/loader!../fixtures/graphql/mutation/createSiteByKey.graphql')
-    deleteSiteByKey = require('graphql-tag/loader!../fixtures/graphql/mutation/deleteSiteByKey.graphql')
-    exportWebsite = require('graphql-tag/loader!../fixtures/graphql/mutation/exportWebsite.graphql')
-    exportAllSites = require('graphql-tag/loader!../fixtures/graphql/mutation/exportAllSites.graphql')
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const createSiteByKey: DocumentNode = require('graphql-tag/loader!../fixtures/graphql/mutation/createSiteByKey.graphql');
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const deleteSiteByKey: DocumentNode = require('graphql-tag/loader!../fixtures/graphql/mutation/deleteSiteByKey.graphql');
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const exportWebsite: DocumentNode = require('graphql-tag/loader!../fixtures/graphql/mutation/exportWebsite.graphql');
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const exportAllSites: DocumentNode = require('graphql-tag/loader!../fixtures/graphql/mutation/exportAllSites.graphql');
 
     before(() => {
-        cy.login()
-    })
+        cy.login();
+    });
 
     it('creates a site via GraphQL and returns true', () => {
         cy.apollo({
@@ -29,11 +28,11 @@ describe('GraphQL Extension Websites', () => {
             }
         })
             .its('data.admin.jahia.createSiteByKey')
-            .should('eq', true)
-        
+            .should('eq', true);
+
         // Cleanup
-        cy.apollo({mutation: deleteSiteByKey, variables: {siteKey: TEST_SITE_KEY}})
-    })
+        cy.apollo({mutation: deleteSiteByKey, variables: {siteKey: TEST_SITE_KEY}});
+    });
 
     it('deletes a site via GraphQL and returns true', () => {
         // First create the site to be deleted
@@ -46,15 +45,15 @@ describe('GraphQL Extension Websites', () => {
                 templateSet: 'default',
                 locale: 'en'
             }
-        })
+        });
 
         cy.apollo({
             mutation: deleteSiteByKey,
             variables: {siteKey: TEST_SITE_KEY}
         })
             .its('data.admin.jahia.deleteSiteByKey')
-            .should('eq', true)
-    })
+            .should('eq', true);
+    });
 
     it('returns false when deleting a non-existent site', () => {
         cy.apollo({
@@ -62,8 +61,8 @@ describe('GraphQL Extension Websites', () => {
             variables: {siteKey: 'non-existent-cypress-site-12345'}
         })
             .its('data.admin.jahia.deleteSiteByKey')
-            .should('eq', false)
-    })
+            .should('eq', false);
+    });
 
     it('exports a website via GraphQL and returns true', () => {
         cy.apollo({
@@ -75,12 +74,12 @@ describe('GraphQL Extension Websites', () => {
             }
         })
             .its('data.admin.jahia.exportWebsite')
-            .should('eq', true)
-    })
+            .should('eq', true);
+    });
 
     it('returns AWS_S3_BUCKET_NOT_CONFIGURED when exportAllSites is called without AWS configuration', () => {
         cy.apollo({mutation: exportAllSites})
             .its('data.admin.jahia.exportAllSites')
-            .should('eq', 'AWS_S3_BUCKET_NOT_CONFIGURED')
-    })
-})
+            .should('eq', 'AWS_S3_BUCKET_NOT_CONFIGURED');
+    });
+});
